6b1611873045f2b3260124b247f26a8e2500a6b1
Umnikova <131268941+Umnikova@users.noreply.github.com>
2023-05-02 15:37:46 +0300

    lvl1_task_1.1 done

---
+++ Homeworks/task_1.1.py
@@ -0,0 +1,10 @@
+my_favorite_songs = ['Waste a Moment', 'Staying\' Alive', 'A Sorta Fairytale', 'Start Me Up', 'New Salvation']
+print(type(my_favorite_songs))
+print(*my_favorite_songs)
+print(len(my_favorite_songs))
+print(my_favorite_songs[0])
+print(my_favorite_songs[4])
+print(my_favorite_songs[1])
+print(my_favorite_songs[-2])
+print(my_favorite_songs[0] +" " + my_favorite_songs[4]+ " " + my_favorite_songs[1]+" "+ my_favorite_songs[-2])
+#print(my_favorite_songs.index('Staying\' Alive'))
