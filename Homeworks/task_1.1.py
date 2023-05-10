9586d6bdd6e38f20dfef1c2e266381621d6c92ae
Umnikova <131268941+Umnikova@users.noreply.github.com>
2023-05-10 15:56:45 +0300

    Delete task_1.1.py

--- Homeworks/task_1.1.py
+++
@@ -1,9 +0,0 @@
-my_favorite_songs = ['Waste a Moment', 'Staying\' Alive', 'A Sorta Fairytale', 'Start Me Up', 'New Salvation']
-#print(len(my_favorite_songs))
-print(my_favorite_songs[0])
-print(my_favorite_songs[4])
-print(my_favorite_songs[1])
-print(my_favorite_songs[-2])
-#Вариант вывода песен в одну строку
-#print(my_favorite_songs[0] +" " + my_favorite_songs[4]+ " " + my_favorite_songs[1]+" "+ my_favorite_songs[-2])
-
